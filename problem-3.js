e86838e30063f295c831786bdaa58b22d87dfde1
Bayezidhosen <bayezidhosen9080@gmail.com>
2026-08-10 00:54:49 +0600

    update problem

---
+++ problem-3.js
@@ -0,0 +1,26 @@
+function countHashtags(caption) {
+    // write your code here
+    
+    if (typeof caption !== "string") {
+        return "Invalid";
+    }
+
+    let words = caption.split(" ");
+    let hashtags = words.filter(word => word.startsWith("#"));
+
+    let hashtagCount = hashtags.length;
+    let longestTag = "";
+
+    for (let hashtag of hashtags) {
+        let tag = hashtag.slice(1);
+
+        if (tag.length > longestTag.length) {
+            longestTag = tag;
+        }
+    }
+
+    return {
+        hashtagCount: hashtagCount,
+        longestTag: longestTag
+    };
+}
